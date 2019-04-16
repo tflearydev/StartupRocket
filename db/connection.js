dcb9f523161223fe74739cc62ff22ec358ad32b2
TFlearyDev <tflearydev@gmail.com>
2019-04-16 16:14:21 -0400

    populated connection file in database folder

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect('mongodb://localhost/idea-board')
+}
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error: ', err)
+    process.exit(-1)
+})
+
+mongoose.connection.once('open', () => {
+    console.log("Mongoose has connected to MongoDB")
+})
+
+module.exports = mongoose
