4a5bbfbadc664af9df6ee9e26b62597d5dc85039
TFlearyDev <tflearydev@gmail.com>
2019-04-17 11:53:56 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const logger = require('morgan')
+const app = express()
+//const userController = require('./controllers/userController.js')
 
 app.use(logger('dev'))
-app.use(express.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
+app.use(express.json())
+app.use(express.static(__dirname + '/client/build/'))
+
+//app.use('/api/users/', userController)
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
 })
 
-const PORT = process.env.PORT || 3001;
+// listening on 3001 for backend
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+  console.log(`App listening on port ${PORT}`)
 })
