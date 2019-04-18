251efb4330b1d24aa248380dc113afbc77e0a1bf
TFlearyDev <tflearydev@gmail.com>
2019-04-18 14:14:15 -0400

    added module.exports = router to startup controller file

--- controllers/startupController.js
+++ controllers/startupController.js
@@ -1,4 +1,6 @@
 const express = require('express')
-const Startup = require('../models/Startup.js')
-const Member = require('../models/Member.js')
+// const Startup = require('../models/Startup.js')
+// const Member = require('../models/Member.js')
 const router = express.Router()
+
+module.exports = router;
