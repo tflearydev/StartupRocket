f6b56b6ac4af1ba6ec06b3e654a2d4551d42fd88
TFlearyDev <tflearydev@gmail.com>
2019-04-18 12:24:14 -0400

    edited a code snippet in server.js to have correct letter casing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const app = express()
-const StartupController = require('./controllers/StartupController.js')
+const startupController = require('./controllers/StartupController.js')
 
 app.use(logger('dev'))
 app.use(express.json())
