bfe766f348d8235eabf555eb8d1b4215708b526a
TFlearyDev <tflearydev@gmail.com>
2019-04-18 16:54:44 -0400

    edit server.js as there are bugs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/client/build/'))
 
 app.use('/api/startups/', startupController)
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/client/build/index.html')
 })
 
