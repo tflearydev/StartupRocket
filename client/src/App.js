e710ae1b66b85915521fda00a00699497145e74e
TFlearyDev <tflearydev@gmail.com>
2019-04-19 01:10:55 -0400

    created home/landing page, now on to the startup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,59 +2,40 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import { Nav } from 'react-bootstrap'
 import { Navbar } from 'react-bootstrap'
-import { Form } from 'react-bootstrap'
-import { FormControl } from 'react-bootstrap'
-import { Button } from 'react-bootstrap'
 import styled from 'styled-components'
 import './App.css'
 import HomePage from './components/HomePage.js'
 import StartupPage from './components/StartupPage.js'
 import MemberPage from './components/MemberPage.js'
 
-
 const StyledLink = styled(Link)`
-margin: 0 auto;
-color: black;
-color: black;
+color: white;
 font-size: 20px;
 `;
-
 class App extends Component {
   render () {
     return (
       <Router>
         <div className='App'>
-          <Navbar bg='light' variant='light'>
+          <Navbar bg='dark' variant='dark'>
             <Navbar.Brand href='/'>
               <img
                 alt=''
-                src='https://unixtitan.net/images/converse-clipart-shoe-jordan-3.png'
-                width='45'
-                height='35'
+                src='https://publicdomainvectors.org/photos/Kliponius-Cartoon-rocket-v5.png'
+                width='50'
+                height='40'
                 className='d-inline-block align-top'
               />
-              StartupRocket
+             {'StartupRocket'}
             </Navbar.Brand>
             <Nav className='mr-auto'>
             <Nav.Link>
-              <StyledLink to='/' style={{ marginLeft: '20px' }}>Home</StyledLink>
-             <StyledLink to='/user' style={{ marginLeft: '20px' }}>Users</StyledLink>
-              <StyledLink to='/#about' style={{ marginLeft: '20px' }}>About</StyledLink>
+              <StyledLink to='/' style={{ marginLeft: '240px' }}>Home</StyledLink>
+             <StyledLink to='/startup' style={{ marginLeft: '110px' }}>Startups</StyledLink>
+              <StyledLink to='/investor' style={{ marginLeft: '110px' }}>Investors</StyledLink>
               </Nav.Link>
             </Nav>
-            <Form inline>
-              <FormControl
-                type='text'
-                placeholder='Search'
-                className='mr-sm-2'
-              />
-              <Button variant='outline-info'>Search</Button>
-            </Form>
           </Navbar>
-
-          <br />
-
-          
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route exact path='/startup' component={StartupPage} />
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+background-color: #004A7F;
+  -webkit-border-radius: 10px;
+  border-radius: 10px;
+  border: none;
+  height: 50px;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Arial;
+  font-size: 25px;
+  padding: 5px 10px;
+  text-align: center;
+  text-decoration: none;
+  -webkit-animation: glowing 1500ms infinite;
+  -moz-animation: glowing 1500ms infinite;
+  -o-animation: glowing 1500ms infinite;
+  animation: glowing 1500ms infinite;
+}
+@-webkit-keyframes glowing {
+  0% { background-color: #0000FF; -webkit-box-shadow: 0 0 3px #0000FF; }
+  50% { background-color: #0000FF; -webkit-box-shadow: 0 0 40px #0000FF; }
+  100% { background-color: #0000FF; -webkit-box-shadow: 0 0 3px #0000FF; }
+}
+
+@-moz-keyframes glowing {
+  0% { background-color: #0000FF; -moz-box-shadow: 0 0 3px #0000FF; }
+  50% { background-color: #0000FF; -moz-box-shadow: 0 0 40px #0000FF; }
+  100% { background-color: #0000FF; -moz-box-shadow: 0 0 3px #0000FF; }
+}
+
+@-o-keyframes glowing {
+  0% { background-color: #0000FF; box-shadow: 0 0 3px #0000FF; }
+  50% { background-color: #0000FF; box-shadow: 0 0 40px #0000FF; }
+  100% { background-color: #0000FF; box-shadow: 0 0 3px #0000FF; }
+}
+
+@keyframes glowing {
+  0% { background-color: #0000FF; box-shadow: 0 0 3px #0000FF; }
+  50% { background-color: #0000FF; box-shadow: 0 0 40px #0000FF; }
+  100% { background-color: #0000FF; box-shadow: 0 0 3px #0000FF; }
+
+`;
+
+const Jumbotron = styled.div`
+  background-image: url('https://www.cloudways.com/wp-content/uploads/2018/04/startup-head-bnr.png');
+  height: 500px;
+`
+
+class HomePage extends Component {
+  render () {
+    return (   
+      /* refactor here and give it a height */
+      <Jumbotron>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+        <StyledLink to='/startup'>Startup Rocket</StyledLink>
+
+      </Jumbotron>
+    
+    )
+  }
+};
+
+export default HomePage
